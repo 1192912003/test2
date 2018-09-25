b11256c2b2dcb513774044c9d38f02a64395bb94
wangyalei <39550719+wyl999@users.noreply.github.com>
2018-09-25 14:38:24 +0800

    Update TestController.java

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -3,5 +3,6 @@ package com.example.demo.controller;
 public class TestController {
     public static void main(String[] args) {
         System.out.println("testController 1 成功");
+        System.out.println("testController 2 成功");
     }
 }
